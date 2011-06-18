235f14b4c10312297cfaed29d45456ba361b2ddf
Steve Purcell <steve@sanityinc.com>
2011-06-18 09:44:48 +0100

    Detect RVM's default IRB prompt in the *console* buffer

--- rinari.el
+++ rinari.el
@@ -247,8 +247,8 @@ argument allows editing of the console command arguments."
     (run-ruby command)
     (save-excursion
       (set-buffer "*ruby*")
-      (set (make-local-variable 'inf-ruby-first-prompt-pattern) "^>> ")
-      (set (make-local-variable 'inf-ruby-prompt-pattern) "^>> ")
+      (set (make-local-variable 'inf-ruby-prompt-pattern) "^\\([a-zA-Z0-9.\-]+ :[0-9]+ >\\|>>\\) ")
+      (set (make-local-variable 'inf-ruby-first-prompt-pattern) inf-ruby-prompt-pattern)
       (rinari-launch))))
 
 (defun rinari-sql ()
