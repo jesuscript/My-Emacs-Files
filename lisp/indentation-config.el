a7128d9738c51d52f5e7d4348bd0cc0a37f648cb
Konstantin <konstantin.k.ed@gmail.com>
2017-08-22 16:42:14 +0100

    added indentation configs

---
+++ lisp/indentation-config.el
@@ -0,0 +1,24 @@
+(setq-default indent-tabs-mode nil
+              c-indent-tabs-mode t
+              c-tab-always-indent t
+              coffee-indent-tabs-mode nil
+              tab-width 2)
+(setq js-indent-level 2)
+(setq js2-basic-offset 2)
+(setq ruby-indent-level 2)
+(setq sgml-basic-offset 2)
+(setq c-basic-offset 2)
+(setq css-indent-offset 2)
+(setq python-indent-offset 2)
+(setq web-mode-markup-indent-offset 4)
+(setq web-mode-code-indent-offset 4)
+(setq web-mode-css-indent-offset 4)
+(setq rust-indent-offset 2)
+(setq elm-indent-offset 2)
+;; (setq js-indent-level 4)
+;; (setq ruby-indent-level 4)
+;; (setq sgml-basic-offset 4)
+;; (setq c-basic-offset 4)
+;; (setq css-indent-offset 4)
+
+(provide 'indentation-config)
